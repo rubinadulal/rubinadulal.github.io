4f415a4d79924514eeec92b2fa5364e41967cfeb
rubinadulal <rubinadulal@mail.weber.edu>
2025-02-06 01:10:54 -0700

    Update scriptss.js

--- scriptss.js
+++ scriptss.js
@@ -25,13 +25,18 @@ function changeMark(buttonId) {
         if (xTurn) {
             document.getElementById(buttonId).innerHTML = "X";
             document.getElementById(buttonId).style.backgroundColor="#799163";
+            document.getElementById(buttonId).style.color="#6F3096";
         }
         else {
             document.getElementById(buttonId).innerHTML = "O";
             document.getElementById(buttonId).style.backgroundColor="#AB3131";
+            document.getElementById(buttonId).style.color="#FF00FF";
         }
 
-    xTurn = !xTurn; //checkWin(); 
+    xTurn = !xTurn; 
+    
+    checkWin(); 
+
     }
 
 }
@@ -66,7 +71,7 @@ function resetG() {
     
 }
 
-function checkW() {
+function checkWin() {
 
 
     let a1 = document.getElementById("a1").innerHTML;
@@ -80,25 +85,80 @@ function checkW() {
     let c3 = document.getElementById("c3").innerHTML;
     
 
-    if (a1 == b1 && b1 == c1 && a1 == "X" || a2 == b2 && b2 == c2 && a2 == "X" || a3 == b3 && b3 == c3 && a3 == "X" ||
-        a1 == a2 && a2 == a3 && a1 == "X" || b1 == b2 && b2 == b3 && b1 == "X" || c1 == c2 && c2 == c3 && c1 == "X" ||
-        a1 == b2 && b2 == c3 && a1 == "X" || c1 == b2 && b2 == a3 && c1 == "X") {
-
+    if ( a1 === b1 && b1 === c1 && a1 === "X" ||
+         a2 === b2 && b2 === c2 && a2 === "X" || 
+         a3 === b3 && b3 === c3 && a3 === "X")
+          {
 
             document.getElementById("win").innerHTML="Result: YAYY!! Player 1 Won!!!";
 
+        return;
+    }
+    
+
+    if ( a1 === a2 && a2 === a3 && a1 === "X" ||
+         b1 === b2 && b2 === b3 && b1 === "X" ||
+          c1 === c2 && c2 === c3 && c1 === "X") 
+          {
+
+            document.getElementById("win").innerHTML="Result: YAYY!! Player 1 Won!!!";
+
+        return;
     }
 
-    if (a1 == b1 && b1 == c1 && a1 == "O" || a2 == b2 && b2 == c2 && a2 == "O" || a3 == b3 && b3 == c3 && a3 == "O" ||
-        a1 == a2 && a2 == a3 && a1 == "O" || b1 == b2 && b2 == b3 && b1 == "O" || c1 == c2 && c2 == c3 && c1 == "O" ||
-        a1 == b2 && b2 == c3 && a1 == "O" || c1 == b2 && b2 == a3 && c1 == "O") {
-            
-            document.getElementById("win").innerHTML="Result: YAYY!! Player 2 Won!!!";
 
+    if ( a1 === b2 && b2 === c3 && a1 === "X" || 
+        c1 === b2 && b2 === a3 && c1 === "X")
+         {
+        document.getElementById("win").innerHTML="Result: YAYY!! Player 1 Won!!!";
+        return;
+    }
+
+    if (a1 === b1 && b1 === c1 && a1 === "O" || 
+        a2 === b2 && b2 === c2 && a2 === "O" || 
+        a3 === b3 && b3 === c3 && a3 === "O") 
+        {
+
+        document.getElementById("win").innerHTML="Result: YAYY!! Player 2 Won!!!";
+
+            return;
+    }
+
+
+    if ( a1 === a2 && a2 === a3 && a1 === "O" ||
+         b1 === b2 && b2 === b3 && b1 === "O" || 
+         c1 === c2 && c2 === c3 && c1 === "O")
+          {
+
+         document.getElementById("win").innerHTML="Result: YAYY!! Player 2 Won!!!";
+
+        return;
+    }
+
+    if (a1 === b2 && b2 === c3 && a1 === "O" ||
+         c1 === b2 && b2 === a3 && c1 === "O")
+         {
+
+       document.getElementById("win").innerHTML="Result: YAYY!! Player 2 Won!!!";
+
+        return;
 
     }
+
+    if (a1 !== "" && b1 !== "" && c1 !== "" &&
+        a2 !== "" && b2 !== "" && c2 !== "" &&
+        a3 !== "" && b3 !== "" && c3 !== "") {
+
+        document.getElementById("win").innerHTML = "Result: URGH!! It is a tie.";
+    }
+
     else {
 
-        document.getElementById("win").innerHTML="Result: URGH!! It is a tie";
+        document.getElementById("win").innerHTML= "";
     }
+   
 }
+
+
+document.getElementById("win").innerHTML="Result: YAYY!! Player 2 Won!!!";
+document.getElementById("win").innerHTML="Result: YAYY!! Player 2 Won!!!";
